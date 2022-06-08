0da71a531b7195892e3d9de424e98f661c8f04d3
Daniil <i.daniil2010@yandex.ru>
2022-06-08 12:23:26 +0400

    mac deploy

--- Rudiron_Terminal.pro
+++ Rudiron_Terminal.pro
@@ -5,6 +5,10 @@ QT += serialport
 # In order to do so, uncomment the following line.
 #DEFINES += QT_DISABLE_DEPRECATED_BEFORE=0x060000    # disables all the APIs deprecated before Qt 6.0.0
 
+macx{
+    CONFIG += app_bundle
+}
+
 SOURCES += \
     Model/iomode.cpp \
     Model/ioptionsmodeldelegate.cpp \
---
+++ deploy_macOS.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+clear
+root_dir=$(PWD)
+echo $root_dir
+
+rm -r release
+mkdir release
+cp -r /Users/daniilignatev/GitHub/build-Rudiron_Terminal-Desktop_Qt_5_12_12_clang_64bit-Release/Rudiron_Terminal.app release/
+cd /Users/daniilignatev/Qt5.12.12/5.12.12/clang_64/bin
+./macdeployqt $root_dir/release/Rudiron_Terminal.app -qmldir=$root_dir/Components -dmg
